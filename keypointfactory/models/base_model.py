Fix incorrect gradient detaching

keypointfactory/models/base_model.py
@@ -165,4 +165,4 @@ class BaseModel(nn.Module, metaclass=MetaModel):
         pass
 
     def _detach_grad_filter(self, key):
-        return True
+        pass